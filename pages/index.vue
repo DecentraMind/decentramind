9fc3ff53273466e00c8aafa838270789a1b24984
HelloRWA <dev@rwa-nft.com>
2024-03-29 19:11:25 +0800

    ✨ add more pages: landing/docs/blog

--- pages/index.vue
+++ pages/index.vue
@@ -1,63 +1,85 @@
 <script setup lang="ts">
-import { sub } from 'date-fns'
-import type { Period, Range } from '~/types'
+definePageMeta({
+  layout: "landing",
+});
 
-const { isNotificationsSlideoverOpen } = useDashboard()
+const { data: page } = await useAsyncData("index", () => queryContent("/").findOne());
+if (!page.value) {
+  throw createError({ statusCode: 404, statusMessage: "Page not found", fatal: true });
+}
 
-const items = [[{
-  label: 'New mail',
-  icon: 'i-heroicons-paper-airplane',
-  to: '/inbox'
-}, {
-  label: 'New user',
-  icon: 'i-heroicons-user-plus',
-  to: '/users'
-}]]
-
-const range = $ref<Range>({ start: sub(new Date(), { days: 14 }), end: new Date() })
-const period = $ref<Period>('daily')
+useSeoMeta({
+  titleTemplate: "",
+  title: page.value.title,
+  ogTitle: page.value.title,
+  description: page.value.description,
+  ogDescription: page.value.description,
+});
 </script>
 
 <template>
-  <UDashboardPage>
-    <UDashboardPanel grow>
-      <UDashboardNavbar title="Home">
-        <template #right>
-          <UTooltip text="Notifications" :shortcuts="['N']">
-            <UButton color="gray" variant="ghost" square @click="isNotificationsSlideoverOpen = true">
-              <UChip color="red" inset>
-                <UIcon name="i-heroicons-bell" class="w-5 h-5" />
-              </UChip>
-            </UButton>
-          </UTooltip>
+  <div v-if="page">
+    <ULandingHero :title="page.hero.title" :description="page.hero.description" :links="page.hero.links">
+      <div class="inset-0 z-[-1] absolute landing-grid [mask-image:radial-gradient(100%_100%_at_top_right,white,transparent)]" />
 
-          <UDropdown :items="items">
-            <UButton icon="i-heroicons-plus" size="md" class="ml-1.5 rounded-full" />
-          </UDropdown>
-        </template>
-      </UDashboardNavbar>
+      <template #headline>
+        <UBadge v-if="page.hero.headline" variant="subtle" size="lg" class="rounded-full font-semibold relative">
+          <NuxtLink :to="page.hero.headline.to" target="_blank" class="focus:outline-none" tabindex="-1">
+            <span class="inset-0 absolute" aria-hidden="true" />
+          </NuxtLink>
 
-      <UDashboardToolbar>
-        <template #left>
-          <!-- ~/components/home/HomeDateRangePicker.vue -->
-          <HomeDateRangePicker v-model="range" class="-ml-2.5" />
+          {{ page.hero.headline.label }}
 
-          <!-- ~/components/home/HomePeriodSelect.vue -->
-          <HomePeriodSelect v-model="period" :range="range" />
-        </template>
-      </UDashboardToolbar>
+          <UIcon v-if="page.hero.headline.icon" :name="page.hero.headline.icon" class="h-4 ml-1 w-4 pointer-events-none" />
+        </UBadge>
+      </template>
+    </ULandingHero>
 
-      <UDashboardPanelContent>
-        <!-- ~/components/home/HomeChart.vue -->
-        <HomeChart :period="period" :range="range" />
+    <ULandingSection class="!pt-0">
+      <Placeholder />
+    </ULandingSection>
 
-        <div class="grid lg:grid-cols-2 lg:items-start gap-8 mt-8">
-          <!-- ~/components/home/HomeSales.vue -->
-          <HomeSales />
-          <!-- ~/components/home/HomeCountries.vue -->
-          <HomeCountries />
+    <ULandingSection
+      v-for="(section, index) in page.sections"
+      :key="index"
+      :title="section.title"
+      :description="section.description"
+      :align="section.align"
+      :features="section.features"
+    >
+      <Placeholder />
+    </ULandingSection>
+
+    <ULandingSection :title="page.features.title" :description="page.features.description">
+      <UPageGrid>
+        <ULandingCard v-for="(item, index) in page.features.items" :key="index" v-bind="item" />
+      </UPageGrid>
+    </ULandingSection>
+
+    <ULandingSection :headline="page.testimonials.headline" :title="page.testimonials.title" :description="page.testimonials.description">
+      <UPageColumns class="xl:columns-4">
+        <div v-for="(testimonial, index) in page.testimonials.items" :key="index" class="break-inside-avoid">
+          <ULandingTestimonial v-bind="testimonial" class="bg-gray-100/50 dark:bg-gray-800/50" />
         </div>
-      </UDashboardPanelContent>
-    </UDashboardPanel>
-  </UDashboardPage>
+      </UPageColumns>
+    </ULandingSection>
+
+    <ULandingSection>
+      <ULandingCTA v-bind="page.cta" class="bg-gray-100/50 dark:bg-gray-800/50" />
+    </ULandingSection>
+  </div>
 </template>
+
+<style scoped>
+.landing-grid {
+  background-size: 100px 100px;
+  background-image: linear-gradient(to right, rgb(var(--color-gray-200)) 1px, transparent 1px),
+    linear-gradient(to bottom, rgb(var(--color-gray-200)) 1px, transparent 1px);
+}
+.dark {
+  .landing-grid {
+    background-image: linear-gradient(to right, rgb(var(--color-gray-800)) 1px, transparent 1px),
+      linear-gradient(to bottom, rgb(var(--color-gray-800)) 1px, transparent 1px);
+  }
+}
+</style>
