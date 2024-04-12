df80b025e3f0a13820671b5b86c750519594ac21
Fiziluuk <gaoqingzhu@industics.com>
2024-04-12 15:16:24 +0800

    任务结算modal

---
+++ components/RuleModal.vue
@@ -0,0 +1,39 @@
+<script lang="ts" setup>
+// import SettlementTask from '~/components/SettlementTask.vue'
+// const modal = useModal()
+
+const emit = defineEmits(['success'])
+const singles = ['单人', '多人']
+const isSingle = ref(singles[0])
+const isTipOpen = ref(false)
+let isSettlementOpen = $ref(false)
+function onClick () {
+  isSettlementOpen=true
+}
+</script>
+
+<template>
+  <UModal v-model="isTipOpen" prevent-close>
+    <UCard>
+      <div v-if="!isSettlementOpen" class="space-y-2">
+        <p>感谢你对社区的支持~请你按照任务规则完成任务，并且将任务URL提交至表格的加号处哦！如果没有遵循任何一条任务规则那么你可能会白费力气喔~</p>
+        <UButton @click="onClick">
+          I know!
+        </UButton>
+      </div>
+      <div v-if="isSettlementOpen">
+        <USelect v-model="isSingle" :options="singles" />
+        <div class="my-8 flex flex-row" v-if="isSingle == '多人'">
+          <div class="mr-4 basis-2/3"><UInput color="primary" variant="outline" placeholder="钱包地址1" /></div>
+          <div class="ml-4 basis-1/3"><UInput color="primary" variant="outline" placeholder="奖励占比" /></div>
+        </div>
+        <div class="my-8 flex flex-row" v-if="isSingle == '多人'">
+          <div class="mr-4 basis-2/3"><UInput color="primary" variant="outline" placeholder="钱包地址2" /></div>
+          <div class="ml-4 basis-1/3"><UInput color="primary" variant="outline" placeholder="奖励占比" /></div>
+        </div>
+        <div class="my-8"><UInput color="primary" variant="outline" placeholder="URL" /></div>
+        <div class="flex justify-center my-8"><UButton>确认提交</UButton></div>
+      </div>
+    </UCard>
+  </UModal>
+</template>
---
+++ pages/wip/settle-task.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import RuleModal from '~/components/RuleModal.vue'
+import CommonAlert from '~/components/CommonAlert.vue'
+
+definePageMeta({
+  layout: "wip",
+});
+const toast = useToast()
+const modal = useModal()
+
+const blogpost = {
+  userId: 1,
+  userName: "gqz",
+  userTwitter: "xx",
+};
+const error_msg = "感谢你对社区的支持~经系统检测，你还没有绑定twitter账号哦！"
+function openModal () {
+  if(isNullOrEmpty(blogpost.userTwitter)){
+    modal.open(CommonAlert, {message: error_msg})
+  }else{
+    modal.open(RuleModal)
+  }
+}
+function isNullOrEmpty(str: string | null | undefined): boolean {
+  return !str || str.length === 0 || str.length == undefined;
+}
+</script>
+
+<template>
+<!--  <div class="bg-red-400">discovery</div>-->
+  <UButton label="Reveal modal" @click="openModal" />
+</template>
