449a0bfdb9b2ca459fa1619555f98b81c40d9251
Fiziluuk <gaoqingzhu@industics.com>
2024-04-16 19:07:52 +0800

    将任务页面迁移至slug

---
+++ pages/[slug]/detail-task.vue
@@ -0,0 +1,198 @@
+<script setup lang="ts">
+
+
+const blogPost = {
+  id: 1,
+  name: "Task 1",
+  image: "https://picsum.photos/640/360",
+  description:
+    "Nuxt 3.9 is out - a Christmas gift from the Nuxt team bringing Vite 5, a new loading API and more." +
+    "Nuxt 3.9 is out - a Christmas gift from the Nuxt team bringing Vite 5, a new loading API and more." +
+    "Nuxt 3.9 is out - a Christmas gift from the Nuxt team bringing Vite 5, a new loading API and more." +
+    "Nuxt 3.9 is out - a Christmas gift from the Nuxt team bringing Vite 5, a new loading API and more." +
+    "Nuxt 3.9 is out - a Christmas gift from the Nuxt team bringing Vite 5, a new loading API and more." +
+    "Nuxt 3.9 is out - a Christmas gift from the Nuxt team bringing Vite 5, a new loading API and more.",
+  reward: "100 Token + 200 USDT",
+  taskTime: "2024/03/02-2024/06/06",
+  rewardSessions: 200,
+  submittedSessions: 195,
+  taskRule:
+    "任务规则任务规则任务规则任务规则任务规则任务规则任务规则任务规则任务规则任务规则任务规则任务规则任务规则" +
+    "任务规则任务规则任务规则任务规则任务规则任务规则任务规则任务规则任务规则任务规则任务规则任务规则任务规则" +
+    "任务规则任务规则任务规则任务规则任务规则任务规则任务规则任务规则任务规则任务规则任务规则任务规则任务规则",
+  builderNum: 100,
+  status: "进行中",
+  isJoin: "已参与",
+};
+
+const columns = [
+  {
+    key: "id",
+    label: "ID",
+  },
+  {
+    key: "wallet",
+    label: "钱包地址",
+  },
+  {
+    key: "brandEffect",
+    label: "品牌效应",
+  },
+  {
+    key: "inviteFriends",
+    label: "呼朋引伴",
+  },
+  {
+    key: "audience",
+    label: "听众",
+  },
+  {
+    key: "url",
+    label: "url",
+  },
+  {
+    key: "score",
+    label: "分数",
+  },
+];
+
+const people = [
+  {
+    id: 1,
+    wallet: "0x120xxxxxx00xxxxxxxxx",
+    brandEffect: 10,
+    inviteFriends: 10,
+    audience: 120,
+    url: "twitter.com/space/1",
+    score: 100,
+  },
+  {
+    id: 2,
+    wallet: "0x120xxxxxx00xxxxxxxxx",
+    brandEffect: 10,
+    inviteFriends: 10,
+    audience: 120,
+    url: "twitter.com/space/1",
+    score: 100,
+  },
+  {
+    id: 3,
+    wallet: "0x120xxxxxx00xxxxxxxxx",
+    brandEffect: 10,
+    inviteFriends: 10,
+    audience: 120,
+    url: "twitter.com/space/1",
+    score: 100,
+  },
+  {
+    id: 4,
+    wallet: "0x120xxxxxx00xxxxxxxxx",
+    brandEffect: 10,
+    inviteFriends: 10,
+    audience: 120,
+    url: "twitter.com/space/1",
+    score: 100,
+  },
+  {
+    id: 5,
+    wallet: "0x120xxxxxx00xxxxxxxxx",
+    brandEffect: 10,
+    inviteFriends: 10,
+    audience: 120,
+    url: "twitter.com/space/1",
+    score: 100,
+  },
+  {
+    id: 6,
+    wallet: "0x120xxxxxx00xxxxxxxxx",
+    brandEffect: 10,
+    inviteFriends: 10,
+    audience: 11,
+    url: "twitter.com/space/1",
+    score: 100,
+  },
+];
+
+const q = ref("");
+
+function switchDisable() {
+  return blogPost.status == '进行中' && blogPost.isJoin == '已参与'
+}
+const filteredRows = computed(() => {
+  if (!q.value) {
+    return people;
+  }
+
+  return people.filter((person) => {
+    return Object.values(person).some((value) => {
+      return String(value).toLowerCase().includes(q.value.toLowerCase());
+    });
+  });
+});
+</script>
+
+<template>
+  <div class="w-full overflow-y-auto h-full  ">
+    <UBlogPost
+      :key="blogPost.id"
+      :image="blogPost.image"
+      :description="blogPost.description"
+      class="m-8"
+    >
+      <template #title>
+        <div class="flex justify-start...">
+          <Text class="mx-3">{{ blogPost.name }}</Text>
+          <div class="mx-8"><UBadge color="green" variant="solid">{{ blogPost.status }}</UBadge></div>
+          <div class="mx-8"><UBadge color="green" variant="solid">{{ blogPost.isJoin }}</UBadge></div>
+          <!-- <UBadge color="green" variant="solid">{{ blogPost.status }}</UBadge>
+          <UBadge color="green" variant="solid">{{ blogPost.isJoin }}</UBadge> -->
+        </div>
+      </template>
+      <template #description>
+        <div class="flex flex-col space-y-2">
+          <Text class="text-blue-900">{{ blogPost.description }}</Text>
+          <div class="flex justify-start ...">
+            <div><Text class=" mr-20 text-blue-300">任务时间:</Text></div>
+            <div>
+              <Text class="text-blue-300">{{ blogPost.taskTime }}</Text>
+            </div>
+          </div>
+          <div class="flex justify-start ...">
+            <div><Text class=" mr-20 text-blue-300">任务奖励:</Text></div>
+            <div>
+              <Text class="text-blue-300">{{ blogPost.reward }}</Text>
+            </div>
+          </div>
+          <div class="flex justify-start ...">
+            <div><Text class=" mr-20 text-blue-300">奖励场次:</Text></div>
+            <div>
+              <Text class="text-blue-300">{{ blogPost.rewardSessions }}</Text>
+            </div>
+          </div>
+          <div class="flex justify-start ...">
+            <div><Text class=" mr-16 text-blue-300">已提交场次:</Text></div>
+            <div>
+              <Text class="text-blue-300">{{ blogPost.submittedSessions }}</Text>
+            </div>
+          </div>
+          <div class="grid grid-rows-2 grid-flow-col gap-4">
+            <div class="mr-16 row-span-2"><Text class="text-blue-300">任务规则:</Text></div>
+            <div class="col-span-2 ...">
+              <Text class="text-blue-300">{{ blogPost.taskRule }}</Text>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div class="flex px-3 py-3.5 border-b border-gray-200 dark:border-gray-700">
+            <Text class=" mr-8 text-blue-300">任务提交排名:</Text>
+            <UInput v-model="q" placeholder="Filter..." />
+          </div>
+          <UTable :rows="filteredRows" :columns="columns" />
+        </div>
+        <div class="flex justify-center my-8" v-if="switchDisable()">
+          <UButton label="结算任务" />
+        </div>
+      </template>
+    </UBlogPost>
+  </div>
+</template>
--- pages/[slug]/tasks.vue
+++ pages/[slug]/tasks.vue
@@ -1,5 +1,129 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const blogPosts = [
+  {
+    id: 1,
+    name: "Task 1",
+    image: "https://picsum.photos/640/360",
+    description:
+      "Nuxt 3.9 is out - a Christmas gift from the Nuxt team bringing Vite 5, a new loading API and more.",
+    reward: "100 Token + 200 USDT",
+    builderNum: 100,
+    status: "未开始",
+  },
+  {
+    id: 2,
+    name: "Task 2",
+    image: "https://picsum.photos/640/360",
+    description: "Nuxt 3.9 is out - a Christmas gift from the Nuxt team bringing Vite 5",
+    reward: "100 Token + 200 USDT",
+    builderNum: 100,
+    status: "未开始",
+  },
+  {
+    id: 3,
+    name: "Task 3",
+    image: "https://picsum.photos/640/360",
+    description: "Nuxt DevTools v1.0 is out, generally available to all Nuxt projects!",
+    reward: "100 Token + 200 USDT",
+    builderNum: 100,
+    status: "未开始",
+  },
+  {
+    id: 4,
+    name: "Task 4",
+    image: "https://picsum.photos/640/360",
+    description: "Nuxt 3.9 is out - a Christmas gift from the Nuxt team bringing Vite 5",
+    reward: "100 Token + 200 USDT",
+    builderNum: 100,
+    status: "未开始",
+  },
+  {
+    id: 5,
+    name: "Task 5",
+    image: "https://picsum.photos/640/360",
+    description:
+      "Nuxt 3.8 is out, bringing built-in DevTools, automatic Nuxt Image install, a new app...",
+    reward: "100 Token + 200 USDT",
+    builderNum: 1000,
+    status: "未开始",
+  },
+  {
+    id: 6,
+    name: "Task 6",
+    image: "https://picsum.photos/640/360",
+    description: "Nuxt 3.9 is out - a Christmas gift from the Nuxt team bringing Vite 5",
+    reward: "100 Token + 200 USDT",
+    builderNum: 100,
+    status: "未开始",
+  },
+  {
+    id: 7,
+    name: "Task 6",
+    image: "https://picsum.photos/640/360",
+    description: "Nuxt 3.9 is out - a Christmas gift from the Nuxt team bringing Vite 5",
+    reward: "100 Token + 200 USDT",
+    builderNum: 100,
+    status: "未开始",
+  },
+];
+
+const items = [
+  {
+    label: "开放任务区",
+    content: "",
+  },
+  {
+    label: "未开放协作区",
+    content: "",
+  },
+];
+
+function onChange(index) {
+  const item = items[index];
+
+  alert(`${item.label} was clicked!此处应该刷新下方列表数据，重新渲染，待完善`);
+}
+</script>
 
 <template>
-  <div class="h-screen bg-red-400 w-full">task</div>
+  <div class="min-h-screen bg-red-1900  overflow-y-auto h-full ml-10 my-10">
+    <div class="flex justify-between mb-8">
+      <div>
+        <UTabs :items="items" @change="onChange" />
+      </div>
+      <div class="flex justify-end ...">
+        <UButton to="/${slug}/create-task/" color="black" variant="solid" size="2xs" >Create Task</UButton>
+      </div>
+    </div>
+    <UBlogList orientation="horizontal">
+      <UBlogPost
+        v-for="blogPost in blogPosts"
+        :key="blogPost.id"
+        :image="blogPost.image"
+        :description="blogPost.description"
+        }
+      >
+        <template #title>
+          <div class="flex justify-between ...">
+            <Text class="mx-3">{{ blogPost.name }}</Text>
+            <UBadge color="green" variant="solid">{{ blogPost.status }}</UBadge>
+          </div>
+        </template>
+        <template #description>
+          <div class="flex flex-col space-y-2">
+            <Text class="text-blue-900">{{ blogPost.description }}</Text>
+            <div class="flex justify-between ...">
+              <div><Text class="text-blue-300">任务奖励:</Text></div>
+              <div><Text class="text-blue-300">{{ blogPost.reward }}</Text></div>
+            </div>
+            <div class="flex justify-between ...">
+              <div><Text class="text-blue-300">已参与人数:</Text></div>
+              <div><Text class="text-blue-300">{{ blogPost.builderNum }}</Text></div>
+            </div>
+          </div>
+        </template>
+        <UButton to="/signup/detail-task/" class="absolute right-0" color="primary" variant="outline">查看详情</UButton>
+      </UBlogPost>
+    </UBlogList>
+  </div>
 </template>
