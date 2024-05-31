7e07066b88a7499330cb64c408047a7ba8ecd70d
Lyu23 <crackyulin@gmail.com>
2024-05-31 17:28:00 +0800

    change ui

--- layouts/default.vue
+++ layouts/default.vue
@@ -189,11 +189,11 @@ const test = ()=> {
           <UIcon name="ion:add" class="h-full w-full " />
         </UButton>
         
-        
+        <!--
         <UButton variant="soft" @click="test">
           <UIcon name="ion:add" class="h-full w-full " />
         </UButton>
-        
+        -->
         <div class="flex-1" />
 
         <UDivider class="bottom-0 sticky" />
