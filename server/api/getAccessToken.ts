b86527a09db273ddf8b7db4af57d9a04bcba6bd2
fizi <1460643849@qq.com>
2024-06-14 08:40:47 +0800

    twitter获取用户id尝试 my wallet显示12位小数

---
+++ server/api/getAccessToken.ts
@@ -0,0 +1,34 @@
+import {auth} from 'twitter-api-sdk'
+
+export default eventHandler(async (event) => {
+  // const { authClient } = $(linktwitter())
+  const STATE = "state";
+  const authClient = new auth.OAuth2User({
+    client_id: "ZkJXajNiRUdwanFQTkZOenZBUzA6MTpjaQ" as string,
+    client_secret: "H29N3gUVa0CwQkZ0Ky9tNqXRu1QzgpISaH9GIGQ5poArbPsdfE" as string,
+    callback: "http://localhost:3000/callback",
+    scopes: ["tweet.read", "users.read", "offline.access"],
+  });
+  // const authUrl = authClient.generateAuthURL({
+  //   state: STATE,
+  //   code_challenge_method: "s256",
+  // });
+  // window.open(authUrl, '_blank');
+  const {code} = getQuery(event) as { code?: string }
+  if(code === 'getToken'){
+    const authUrl = authClient.generateAuthURL({
+      state: STATE,
+      code_challenge_method: "plain",
+      code_challenge: "test",
+    });
+    // window.open(authUrl, '_blank');
+     // 替换为你要跳转的 URL
+    // 返回重定向响应
+    // return { statusCode: 302, headers: { Location: authUrl } }
+    return authUrl
+
+  }else{
+
+    const res = await authClient.requestAccessToken(code as string)
+  }
+})
