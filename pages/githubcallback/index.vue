66ca178ecd1c31071b9b65860237ad28a50508a2
Lyu23 <crackyulin@gmail.com>
2024-06-23 21:56:33 +0800

    change github connect

--- pages/githubcallback/index.vue
+++ pages/githubcallback/index.vue
@@ -19,13 +19,13 @@ import { useRouter } from 'vue-router'
 const toast = useToast()
 const router = useRouter()
 let getGithub = $ref(false)
-let githubsecret = $ref()
 const { userInfo, getInfo, personalGithub } = $(aocommunityStore())
 const { github } = $(linktwitter())
+
+const user = useSupabaseUser()
+
 const test = async() => {
-    console.log(githubsecret)
-
-
+    console.log(user.value.user_metadata.provider_id)    
 }
 const backCommunity = async() => {
     router.push('/dm')
@@ -35,11 +35,12 @@ onMounted(async () => {
     const state = new URLSearchParams(window.location.search).get('state')
     
     await getInfo()
+    
     const connectGithub = "Success"
-
+    //const connectGithub = user.value.user_metadata.provider_id
     if (code && state === 'state') {
         try {
-            const githubCode = await personalGithub(
+            const githubCode = await personalInfo(
                 userInfo[0].avatar,
                 userInfo[0].name,
                 userInfo[0].twitter,
