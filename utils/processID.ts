f78add30a997f205b8ccb4549cacb798fd89b62c
vico <ray7551@gmail.com>
2024-07-24 14:44:42 +0800

    chore: update moduleID

--- utils/processID.ts
+++ utils/processID.ts
@@ -3,7 +3,8 @@ export const aoCommunityProcessID = 'jl0nyTKNDHPVMoE3DlaHiBnn8Ltoz-x0zJ2Qytag9qU
 export const tasksProcessID = 'mNbAy9OY-R0tdrNnQBQatVKo37SxlY7bt-ouS_hZ33w'
 
 // TODO Array of moduleID. Try different moduleID when current module not work.
-// alternative moduleID: 5l00H2S0RuPYe-V5GAI-1RgQEHFInSMr20E-3RNXJ_U
-export const moduleID = 'SBNb1qPQ1TDwpD_mboxm2YllmMLXpWw4U8P9Ff8W9vk'
+// export const moduleID = 'SBNb1qPQ1TDwpD_mboxm2YllmMLXpWw4U8P9Ff8W9vk'
+export const moduleID = 'AQhN6EY8jbwcd9YygzN1JvuaV_E86lHB-rjkQ7X3Omg'
+// export const moduleID = '5l00H2S0RuPYe-V5GAI-1RgQEHFInSMr20E-3RNXJ_U'
 
 export const schedulerID = '_GQ33BkPtZrqxA84vM8Zk-N2aO0toNNu_C-l-rawrBA'
