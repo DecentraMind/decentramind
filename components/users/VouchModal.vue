064f0ce3737d50645cb9ea276f77eb80e18b58d5
vico <ray7551@gmail.com>
2024-11-30 04:00:04 +0800

    update vouch modal

---
+++ components/users/VouchModal.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+const isOpen = ref(false)
+</script>
+<template>
+  <UModal v-model="isOpen">
+    <UCard
+      class="h-fit flex flex-col items-center justify-start"
+      :ui="{
+        header: {
+          padding: 'py-4',
+        },
+        body: {
+          base: 'w-full',
+        },
+      }"
+    >
+      <template #header>
+        <h3 class="font-semibold leading-6 text-gray-900 dark:text-white">
+          Vouch Required
+        </h3>
+      </template>
+      <div class="w-full flex-center gap-x-4">
+        <UButton
+          color="gray"
+          variant="ghost"
+          title="I'm vouched, reload page"
+          @click="reloadNuxtApp()"
+        >
+          I'm vouched
+        </UButton>
+        <NuxtLink
+          to="https://g8way.io/Cikp3X7Zk4cI1RtBEq-pVh_fhz-npd5dZ5-0EgCxTQM"
+          target="_blank"
+        >
+          <UButton icon="heroicons:arrow-top-right-on-square">
+            Get Vouched
+          </UButton>
+        </NuxtLink>
+      </div>
+    </UCard>
+  </UModal>
+</template>
