3ff645b5387b65d777e9ad9ac710ae0d6b0a4b84
vico <ray7551@gmail.com>
2025-01-01 04:12:53 +0800

    wallet switch event needs ACCESS_ALL_ADDRESSES permission

--- stores/aoStore.ts
+++ stores/aoStore.ts
@@ -14,6 +14,7 @@ import type { PermissionType } from 'arconnect'
 
 const permissions: PermissionType[] = [
   'ACCESS_ADDRESS',
+  'ACCESS_ALL_ADDRESSES', // wallet switch event needs this permission
   'SIGNATURE',
   'SIGN_TRANSACTION',
   'DISPATCH',
