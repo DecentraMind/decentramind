9cad54513aa8df31da1b16bc9923525bd9190c49
vico <ray7551@gmail.com>
2025-03-11 18:02:39 +0800

    get vouch data from AO only

--- utils/user/vouch.client.ts
+++ utils/user/vouch.client.ts
@@ -2,73 +2,6 @@ import type { VouchData } from '~/types'
 import { uniq } from 'lodash-es'
 import { messageResult } from '~/utils/ao'
 import { VALID_VOUCHERS } from '~/utils/processID'
-import { GQL_ENDPOINT } from '~/utils/arwaeve/arwaeve'
-
-export async function getVouchDataGQL(address: string, method: string = 'X') {
-  if (!address) {
-    throw new Error('No address specified.')
-  }
-  const validVouchers = VALID_VOUCHERS
-    .map(voucher => `"${voucher}"`)
-    .join(',')
-  const gql = /* GraphQL */ `
-    query {
-      transactions(
-        owners: [${validVouchers}],
-        tags: [
-          {name: "Data-Protocol", values: ["Vouch"]},
-          {
-            name: "Vouch-For"
-            values: ["${address}"]
-          }
-          { name: "Method", values: "${method}" }
-        ]
-      ) {
-        edges {
-          node {
-            id
-            owner {
-              address
-              key
-            }
-            tags {
-              name
-              value
-            }
-          }
-        }
-      }
-    }
-  `
-  // fetch from GQL_ENDPOINT
-  const response = await fetch(GQL_ENDPOINT, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ query: gql }),
-  })
-  const data = await response.json()
-  console.log('vouch data from gql endpoint:', data.data)
-
-  if (data.errors && data.errors.length > 0) {
-    console.error('Error fetching vouch data from gql endpoint:', data.errors)
-    throw new Error('Error fetching vouch data from gql endpoint')
-  }
-
-  if (!data.data?.transactions?.edges?.length) {
-    console.log('No vouch data found.')
-    return []
-  }
-
-  const twitterVouchedIDs: string[] = []
-  for (const edge of data.data.transactions.edges) {
-    for (const tag of edge.node.tags) {
-      if (tag.name === 'Identifier') {
-        twitterVouchedIDs.push(tag.value)
-      }
-    }
-  }
-  return uniq(twitterVouchedIDs)
-}
 
 /** move this function to aoStore */
 export const getVouchData = async (address: string, method: string = 'X'): Promise<string[]> => {
@@ -119,17 +52,4 @@ export const getVouchData = async (address: string, method: string = 'X'): Promi
   console.log('twitterVouchedIDs:', twitterVouchedIDs)
 
   return twitterVouchedIDs
-}
-
-/**
- * get vouch data from gql endpoint, if it fails, try to get it from AO
- * @returns twitterVouchedIDs string[] vouched handles for this address
- */
-export const getVouchDataSafe = async (address: string, method: string = 'X'): Promise<string[]> => {
-  try {
-    return await getVouchDataGQL(address, method)
-  } catch (e) {
-    console.warn('Failed to get vouch data from gql endpoint, trying to get it from AO', e)
-    return await getVouchData(address, method)
-  }
 }
